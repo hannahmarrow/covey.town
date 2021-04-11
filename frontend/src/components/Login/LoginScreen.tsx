cf2f94c9621d8fc9f23299bab06890b53a348f37
jeffy-zhu <zjeff.jz@gmail.com>
2021-04-11 17:36:34 -0400

    added styling for LoginScreen component

--- frontend/src/components/Login/LoginScreen.tsx
+++ frontend/src/components/Login/LoginScreen.tsx
@@ -1,12 +1,32 @@
 import React, { useState } from 'react';
 import { Box, Heading, Text, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
 import firebase from 'firebase';
+import { makeStyles, Theme } from '@material-ui/core';
 import CreateAccount from './CreateAccount';
 
+const useStyles = makeStyles((theme: Theme) => ({
+  container: {
+    margin: 'auto',
+    width: '50%'
+  },
+  emailField: {
+    width: '75%',
+  },
+  passwordField: {
+    width: '75%',
+  },
+  continueAsGuestBtn: {
+    margin: '0 0 5px 0',
+  },
+  loginInBtn: {
+    margin: '5px 0',
+  },
+}));
 
 export default function LoginScreen(): JSX.Element {
     const [email, setEmail] = useState<string>('');
     const [password, setPassword] = useState<string>('');
+    const classes = useStyles();
 
     const continueAsGuest = () => {
       firebase.auth().signInAnonymously()
@@ -17,27 +37,38 @@ export default function LoginScreen(): JSX.Element {
     };
 
     return (
+      <div className={classes.container}>
         <Box p="4" borderWidth="1px" borderRadius="lg">
             <Heading as="h2" size="xl">Welcome to Covey.Town!</Heading>
             <Text>A new, open source app for hanging out with your friends and meeting new people</Text>
             <Text>Please log in with your email and password, or join as a guest.</Text>
+            <br/>
             <FormControl>
               <FormLabel htmlFor="emailTextbox">Email</FormLabel>
-              <Input autoFocus name="emailTextbox" placeholder="Your Email"
-                     value={email}
-                     onChange={event => setEmail(event.target.value)}
-              />
-              <Button size="xs" onClick={continueAsGuest}>Continue As Guest</Button>
+              <div className={classes.emailField}>
+                <Input autoFocus name="emailTextbox" placeholder="Your Email"
+                        value={email}
+                        onChange={event => setEmail(event.target.value)}
+                />
+              </div>
+              <div className={classes.continueAsGuestBtn}>
+                <Button size="xs" onClick={continueAsGuest}>Continue As Guest</Button>
+              </div>
             </FormControl>
             <FormControl>
               <FormLabel htmlFor="passwordTextBox">Password</FormLabel>
-              <Input autoFocus name="passwordTextBox" placeholder="Your password"
-                     value={password}
-                     onChange={event => setPassword(event.target.value)}
-              />
+              <div className={classes.passwordField}>
+                <Input autoFocus name="passwordTextBox" placeholder="Your password"
+                        value={password}
+                        onChange={event => setPassword(event.target.value)}
+                />
+              </div>
             </FormControl>
-            <Button colorScheme="blue" onClick={login}>Log In</Button>
+            <div className={classes.loginInBtn}>
+              <Button colorScheme="blue" isFullWidth onClick={login}>Log In</Button>
+            </div>
             <CreateAccount/>
         </Box>
+      </div>
     )
 }
