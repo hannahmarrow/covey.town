422727bf1a614c2ee1935a8652b63b97689f540e
KeithCes <ktc502@gmail.com>
2021-04-16 19:49:38 -0400

    accidently removed testing library, lets add that back

--- frontend/src/components/Login/Firebase.test.tsx
+++ frontend/src/components/Login/Firebase.test.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
+import '@testing-library/jest-dom'
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
